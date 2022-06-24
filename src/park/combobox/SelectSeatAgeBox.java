558cdc46c580ee38a28d0853fa21a1bc4a6b0610
Gi7D-08 <Gi7D-08@DESKTOP-THCB3SC>
2022-06-24 13:49:32 +0900

    0624 연령 고르는 콤보박스 수정한 필드 값 기준으로 실행 성공

--- src/park/combobox/SelectSeatAgeBox.java
+++ src/park/combobox/SelectSeatAgeBox.java
@@ -19,18 +19,20 @@ public class SelectSeatAgeBox extends JComboBox<String>{ // 연령별 좌석개수를 고
 	String age;
 	int seatCnt;
 	double discountRate=0;
+	String seatName;
 	//Integer seatKey = user.getSeatKeys().get(seatCnt); // 체크박스 나오는 순서에 해당하는 자석번호
 	
 	public SelectSeatAgeBox(ReservationInfo user, int seatCnt) { // 예매정보에 연령 좌석 추가해야해서 클래스 그대로 이끌어오고 어떤 좌석에 대한 콤보박스인지 확인해야해서 배열 인덱스로 쓸 seatnum 데려온다
 		this.user = user;
 		this.seatCnt = seatCnt;
-		//Integer seatKey = user.getSeatKeys().get(seatCnt);
+		seatName = user.getSeatNames().get(seatCnt);
 		
-		addItem(String.format("%d번 좌석", user.getSeatKeys().get(seatCnt))); // i번째 키 값에 해당하는 좌석 번호를 가장 위에 표시
+		addItem(String.format("%s번 좌석", user.getSeatNames().get(seatCnt))); // i번째 키 값에 해당하는 좌석 번호를 가장 위에 표시
 		addItem("------");
 		for(int i=0;i<ageGroup.length;i++) { // 성인 청소년 어린이 추가
 			addItem(ageGroup[i]);
 		}
+		setName(seatName);
 		setBackground(Color.white);
 		setSize(10, 10);
 		selectSeat();
@@ -41,12 +43,13 @@ public class SelectSeatAgeBox extends JComboBox<String>{ // 연령별 좌석개수를 고
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				age = (String) getSelectedItem().toString(); // 콤보박스에서 고른 값을 string으로 변환
-				if(age.equals("------")||age.equals(String.format("%d번 좌석", user.getSeatKeys().get(seatCnt)))) {// 키 값을 고르거나 ---- 고르면 다시 선택하게
+				if(age.equals("------")||age.equals(String.format("%s번 좌석", user.getSeatNames().get(seatCnt)))) {// 키 값을 고르거나 ---- 고르면 다시 선택하게
 					new JOptionPane().showMessageDialog(null, "성인, 청소년, 아동 중 선택하세요.","오류",JOptionPane.ERROR_MESSAGE); // 성인 어린이 청소년 제외 선택하면 오류창 뜨게
 				}else {
 					discount(age);
-					user.getSeatName().put(user.getSeatKeys().get(seatCnt), discountRate); // 해당 좌석번호 자리에 할인율 넣는다
-					System.out.println(user.getSeatName().get(user.getSeatKeys().get(seatCnt))+" 저장 완료 " ); // 해쉬맵에 저장되는지 확인
+					user.getdcBySeat().put(user.getSeatNameBySeatId().get(seatName), discountRate);  // 해당 좌석번호 자리에 할인율 넣는다
+					System.out.println(user.getdcBySeat().get(user.getSeatNameBySeatId().get(seatName))+" 저장 완료 " ); // 해쉬맵에 저장되는지 확인
+
 				}
 			}
 		});
@@ -55,13 +58,13 @@ public class SelectSeatAgeBox extends JComboBox<String>{ // 연령별 좌석개수를 고
 	void discount(String age) {
 		if(age.equals("성인")) {
 			discountRate = 1;
-			user.setAgeBySeat(user.getSeatKeys().get(seatCnt), age);
+			user.setAgeBySeat(user.getSeatNameBySeatId().get(seatName), age);
 		}else if(age.equals("청소년")) {
 			discountRate = 0.85;
-			user.setAgeBySeat(user.getSeatKeys().get(seatCnt), age);
+			user.setAgeBySeat(user.getSeatNameBySeatId().get(seatName), age);
 		}else if(age.equals("아동")){
 			discountRate = 0.7;
-			user.setAgeBySeat(user.getSeatKeys().get(seatCnt), age);
+			user.setAgeBySeat(user.getSeatNameBySeatId().get(seatName), age);
 		}
 	}
 }
