9f4b3e1021960c10716d5b4ac8a8988bdbea3cc8
leemicha0929 <leemicha0929@DESKTOP-857KTPH>
2022-06-28 14:00:52 +0900

    자잘한거 디자인 수정

--- src/lee/mpcomponents/MPleavePanel.java
+++ src/lee/mpcomponents/MPleavePanel.java
@@ -10,7 +10,7 @@ import javax.swing.JPasswordField;
 public class MPleavePanel extends JPanel {
 	
 	
-	JLabel MPleaveConfirmLb = new JLabel("<html>계정 탈퇴를 원하시면 비밀번호를 입력해주세요.</html>");
+	JLabel MPleaveConfirmLb = new JLabel("계정 탈퇴를 원하시면 비밀번호를 입력해주세요.");
 	JButton MPleaveYesBtn = new JButton("확인");
 	JPasswordField MPleavePf = new JPasswordField();
 	
@@ -19,19 +19,19 @@ public class MPleavePanel extends JPanel {
 		setLayout(null);
 		
 		//탈퇴하시겠습니까? 라벨 설정
-		MPleaveConfirmLb.setBounds(30, 30, 300, 60);
+		MPleaveConfirmLb.setBounds(50, 30, 400, 60);
 		MPleaveConfirmLb.setOpaque(true);
 		MPleaveConfirmLb.setFont(new Font("고딕", Font.BOLD, 15));
 		add(MPleaveConfirmLb);
 
 		JLabel MPleavePwLb = new JLabel();
 		MPleavePwLb.setText("비밀번호 ");
-		MPleavePwLb.setBounds(30, 100, 100, 27);
+		MPleavePwLb.setBounds(50, 100, 100, 27);
 		MPleavePwLb.setFont(new Font("고딕", Font.BOLD, 15));
 		add(MPleavePwLb);
 		
 		//비밀번호 텍스트필드 설정
-		MPleavePf.setBounds(100, 100, 250, 27);
+		MPleavePf.setBounds(120, 100, 250, 27);
 		MPleavePf.setEchoChar('●');
 		add(MPleavePf);
 		
